d9d195151d3cada77d8968fec99cf295c80ef709
Somodeep Goswami <somodeepgoswami41@gmail.com>
2026-07-02 17:10:36 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -11,9 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://research-paper-gap-finder.vercel.app/"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
